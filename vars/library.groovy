c71e826959ec973feb23a96d8e34c34cd13526b9
sergeakak <96155428+sergeakak@users.noreply.github.com>
2023-01-21 22:24:34 -0600

    Update library.groovy

--- vars/library.groovy
+++ vars/library.groovy
@@ -7,3 +7,8 @@ def newMaven()
 {
   sh 'mvn package'
 }
+
+def newDeploy(ip,appname)
+{
+  deploy adapters: [tomcat9(credentialsId: '986ae753-77fb-4d82-86de-010a9bc00d3c', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
+}
