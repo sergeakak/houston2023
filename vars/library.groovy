05efc029fb7c8f445aadb92b3efb877b8404bb68
sergeakak <96155428+sergeakak@users.noreply.github.com>
2023-01-21 22:56:40 -0600

    Update library.groovy

--- vars/library.groovy
+++ vars/library.groovy
@@ -12,3 +12,8 @@ def newDeployment(ip,appname)
 {
   deploy adapters: [tomcat9(credentialsId: '986ae753-77fb-4d82-86de-010a9bc00d3c', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
 }
+
+def newSelenium(path)
+{
+  sh 'java -jar "${path}"testing.jar'
+}
